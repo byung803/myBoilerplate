944d9a9e9851ba13be1e049e6532f0c8d47de5e1
최병민(01045842518) <byung803@gmail.com>
2018-06-24 19:32:14 +0900

    my boilerplate v1 - first commit

---
+++ src/router.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Router extends Component {
+    render() {
+        return (
+            <div>Router</div>
+        );
+    }
+}
+
+export default Router; 

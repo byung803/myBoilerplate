ba68e5a9ad03a387f17db38407ed2f67cf8f8f86
BYUNGMIN CHOI <byung803@gmail.com>
2018-10-16 16:34:51 +0900

    add redux store basic environment

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducer from './reducers';
 import Router from './router';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 
+const store = createStore(reducer);
+console.log(store.getState());
 
-ReactDOM.render(<Router />, document.getElementById('app'));
+ReactDOM.render(
+    <Provider store={store} >
+        <Router />
+    </Provider>
+    , document.getElementById('app'));
